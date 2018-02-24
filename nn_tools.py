7782adbbb546ec7d9da43c8712744153b26c8325
kulgod <akulgod@gmail.com>
2018-02-24 13:07:49 -0800

    fixed nn_tools

--- nn_tools.py
+++ nn_tools.py
@@ -15,19 +15,19 @@ def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
     mini_batches -- list of synchronous (mini_batch_X, mini_batch_Y)
     """
 
-    m = X.shape[1]                  # number of training examples
+    m = X.shape[0]                  # number of training examples
     mini_batches = []
 
     # Step 1: Shuffle (X, Y)
     permutation = list(np.random.permutation(m))
-    shuffled_X = X[:, permutation]
-    shuffled_Y = Y[:, permutation].reshape((1,m))
+    shuffled_X = X[permutation, :]
+    shuffled_Y = Y[permutation, :].reshape((m,2))
 
     # Step 2: Partition (shuffled_X, shuffled_Y). Minus the end case.
     num_complete_minibatches = math.floor(m/mini_batch_size) # number of mini batches of size mini_batch_size in your partitionning
     for k in range(0, num_complete_minibatches):
-        mini_batch_X = shuffled_X[:, mini_batch_size*k : mini_batch_size * (k+1)]
-        mini_batch_Y = shuffled_Y[:, mini_batch_size*k : mini_batch_size * (k+1)]
+        mini_batch_X = shuffled_X[mini_batch_size*k : mini_batch_size * (k+1), :]
+        mini_batch_Y = shuffled_Y[mini_batch_size*k : mini_batch_size * (k+1), :]
 
         mini_batch = (mini_batch_X, mini_batch_Y)
         mini_batches.append(mini_batch)
@@ -35,8 +35,8 @@ def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
     # Handling the end case (last mini-batch < mini_batch_size)
     if m % mini_batch_size != 0:
 
-        mini_batch_X = shuffled_X[:, mini_batch_size*num_complete_minibatches:]
-        mini_batch_Y = shuffled_Y[:, mini_batch_size*num_complete_minibatches:]
+        mini_batch_X = shuffled_X[mini_batch_size*num_complete_minibatches:, :]
+        mini_batch_Y = shuffled_Y[mini_batch_size*num_complete_minibatches:, :]
 
         mini_batch = (mini_batch_X, mini_batch_Y)
         mini_batches.append(mini_batch)
